ec160f9f28024ecac21448fba5c21645de919191
mooseCoder527 <123958914+mooseCoder527@users.noreply.github.com>
2026-02-26 19:56:40 +0100

    added gold block that has 1 in 50 chance to spawn instead of stone

--- src/main/java/blockcraft/DesktopLauncher.java
+++ src/main/java/blockcraft/DesktopLauncher.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public final class DesktopLauncher {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("BlockCraft 2D");
+        config.setTitle("BlockCraft demo");
         config.setWindowedMode(1280, 720);
         config.useVsync(true);
         config.setForegroundFPS(60);
